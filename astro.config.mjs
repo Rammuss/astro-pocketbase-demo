9dd943d79ec2415898499c67a12b53a6de80e6d0
Rammuss <mmarcoscaceres@gmail.com>
2026-05-21 20:51:59 -0300

    Habilita allowedHosts en server y preview

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   output: "static",
   vite: {
+    server: {
+      allowedHosts: true
+    },
     preview: {
       allowedHosts: true
     }
